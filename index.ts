dbb4b46d8a62b1d8c37d4e68fdcc9fa158d439dc
Anwesh43 <anweshthecool0@gmail.com>
2020-12-19 20:37:56 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight
-const parts : number = 5 
+const parts : number = 2 
+const lines : number = 5 
 const scGap : number = 0.02 
 const strokeFactor : number = 90 
 const sizeFactor : number = 4.9 
@@ -26,5 +27,46 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBarLineDivider(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const gap : number = (2 * size) / lines 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.translate(-size / 2 + (size) * j, -size)
+            DrawingUtil.drawLine(context, 0, -2 * size * sc2, 0, -2 * size * sc1)
+            context.restore()
+        }
+        for (var j = 0; j < lines; j++) {
+            const sc1j : number = ScaleUtil.divideScale(sc1, j, lines)
+            const sc2j : number = ScaleUtil.divideScale(sc2, j, parts)
+            context.save()
+            context.translate(-size / 2, -size - gap * j)
+            DrawingUtil.drawLine(context, size * sc2j, 0, size * sc1j, 0)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBLDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBarLineDivider(context, scale)
     }
 }
